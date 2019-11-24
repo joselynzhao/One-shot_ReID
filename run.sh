a109a1aee5ad1036b20ba0779af565c09506469a
joselynzhao <1326549593@qq.com>
2019-11-24 10:37:46 +0800

    ¡add run3.py

--- run.sh
+++ run.sh
@@ -17,10 +17,10 @@
 #python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_1/  --yita 1  --mode Dissimilarity --max_frames 900 --device 0  -b 128  #验证作者的方法
 #python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_100/  --yita 100  --mode Dissimilarity --max_frames 900 --device 1 -b 128
 #python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_200/  --yita 200  --mode Dissimilarity --max_frames 900 --device 2  -b 128
-python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_300_10/  --yita 300  --mode Dissimilarity --max_frames 900  -b 128  --device 6
-#python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_600/  --yita 600  --mode Dissimilarity --max_frames 900  -b 128  --device 4
+#python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_300_10/  --yita 300  --mode Dissimilarity --max_frames 900  -b 128  --device
+#python3.6 run3.py --dataset mars --logs_dir logs/mars_yita70JZ_1/  --yita 1  --mode Dissimilarity --max_frames 900  -b 128  --device 0 --resume logs/mars_yita70JZ_1/
 
-#python3.6 run_gradully.py --dataset mars --logs_dir logs/mars_gradually20_0/  --EF 5 --q  1 --mode Dissimilarity --max_frames 900 --device 5  -b 128 #验证作者的方法
+python3.6 run_gradully.py --dataset mars --logs_dir logs/mars_gradually20_0_96/  --EF 5 --q  1 --mode Dissimilarity --max_frames 900 --device 5  -b 96 --resume logs/mars_gradually20_0_96/
 #python3.6 run_gradully.py --dataset mars --logs_dir logs/mars_gradually20_1/  --EF 1.1 --q  1.5 --mode Dissimilarity --max_frames 900 --device 4  -b 128 #验证作者的方法
 #python3.6 run_gradully.py --dataset mars --logs_dir logs/mars_gradually20_2/  --EF 0.055 --q  2.5 --mode Dissimilarity --max_frames 900 --device 5  -b 128 #验证作者的方法
 #python3.6 run_gradully.py --dataset mars --logs_dir logs/mars_gradually20_3/  --EF 22.3 --q  0.5 --mode Dissimilarity --max_frames 900 --device 6  -b 128 #验证作者的方法
--- run3.py
+++ run3.py
@@ -132,7 +132,7 @@ def main(args):
 
     nums_to_select = 0
     new_train_data = l_data
-    step = 1
+    step = 19
     # to_list = []
     step_size = []
     base_step = args.bs
@@ -185,9 +185,9 @@ def main(args):
         # onetimeE =time.time()
         # onetime = onetimeE-onetimeS
         # h, m, s = changetoHSM(onetime)
-        data_file.write("step:{} top1:{:.2%} nums_selected:{} selected_percent:{:.2%} mAP:{:.2%} label_pre:{:.2%} select_pre:{:.2%}\n".format(int(step),top1,step_size[step-1],step_size[step-1]/len(u_data),mAP,label_pre,select_pre))
+        data_file.write("step:{} top1:{:.2%} nums_selected:{} selected_percent:{:.2%} mAP:{:.2%} label_pre:{:.2%} select_pre:{:.2%}\n".format(int(step),top1,nums_to_select,nums_to_select/len(u_data),mAP,label_pre,select_pre))
         # time_file.write("step:{} traning:{:.8} evaluate:{:.8} estimate:{:.8} onetime:{:.8}\n".format(int(step),onetime_train,onetime_evaluate,onetime_estimate,onetime))
-        print("step:{} top1:{:.2%} nums_selected:{} selected_percent:{:.2%} mAP:{:.2%} label_pre:{:.2%} select_pre:{:.2%}".format(int(step),top1,step_size[step-1],step_size[step-1]/len(u_data),mAP,label_pre,select_pre))
+        print("step:{} top1:{:.2%} nums_selected:{} selected_percent:{:.2%} mAP:{:.2%} label_pre:{:.2%} select_pre:{:.2%}".format(int(step),top1,nums_to_select,nums_to_select/len(u_data),mAP,label_pre,select_pre))
         # print("onetime cost %02d:%02d:%02.6f" % (h, m, s))
         step = step + 1
 
